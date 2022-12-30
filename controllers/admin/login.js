a36647ebfe6c9303fb3d474d30bec54cf76bb36a
smartguy205 <absolutewithmail@gmail.com>
2022-12-30 21:29:58 +0530

    new changes

--- controllers/admin/login.js
+++ controllers/admin/login.js
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 const login = async (email, password, res) => {
   try {
     const admin = await adminModal.findOne({ email }, { password: 1 });
-
     const isPasswordCorrect = await bcrypt.compare(password, admin.password);
 
     if (!admin || isPasswordCorrect === false) {
