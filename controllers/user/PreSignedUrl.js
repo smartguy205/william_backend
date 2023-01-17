482c8f943d4ca10aa1b345da26399087544c1de8
smartguy205 <absolutewithmail@gmail.com>
2023-01-17 19:14:08 +0530

    preSignedUrl config

--- controllers/user/PreSignedUrl.js
+++ controllers/user/PreSignedUrl.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 AWS.config.update({
-    accessKeyId: "AKIAQTMVOS2PEAPSAOPA",
-    secretAccessKey: "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"
+    accessKeyId: process.env.S3_ACCESS_ID,
+    secretAccessKey: process.env.S3_SECRET_KEY
 });
 
 const s3 = new AWS.S3({
